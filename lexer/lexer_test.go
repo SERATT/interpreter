c7bcff122a528be011e77b10f4ba6bb614034e9c
SERATT <serat5915@gmail.com>
2024-07-20 22:01:40 +0500

    tokens defined

---
+++ lexer/lexer_test.go
@@ -0,0 +1,22 @@
+package lexer
+
+import "interpreter/token"
+
+func TestNextToken() {
+	input := "=+(){}.;"
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+}
