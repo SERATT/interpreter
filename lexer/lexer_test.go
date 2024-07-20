13894c18087d9f9663cf7bb2d488431b84d5dd45
SERATT <serat5915@gmail.com>
2024-07-20 22:02:06 +0500

    lexer test written

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,6 +1,9 @@
 package lexer
 
-import "interpreter/token"
+import (
+	"interpreter/token"
+	_ "testing"
+)
 
 func TestNextToken() {
 	input := "=+(){}.;"
@@ -17,6 +20,20 @@ func TestNextToken() {
 		{token.COMMA, ","},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
 	}
 
 }
